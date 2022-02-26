85675538b8ce95f4d2663cb5d8cbba8b7e01a522
hubcityhubcity <100476036+hubcityhubcity@users.noreply.github.com>
2022-02-26 14:59:32 -0600

    Initialize fix-transaction-item-in-extension

---
+++ src/components/transactions/TransactionMetadata.vue
@@ -0,0 +1,109 @@
+<template>
+  <div class="tx__metadata">
+    <p>
+      Block
+      <router-link
+        v-if="checkFeatureAvailable()"
+        :to="{ name: `block`, params: { height: transaction.height } }"
+      >
+        #{{ transaction.height | prettyInt }}</router-link
+      >
+      <span v-else>#{{ transaction.height | prettyInt }}</span>
+      &nbsp;<i class="material-icons notranslate">access_time</i>&nbsp;{{
+        date
+      }}
+    </p>
+    <p v-if="transaction.undelegationEndTime">
+      Liquid date:
+      {{ getUndelegationEndTime() }}
+    </p>
+    <p v-if="transaction.memo">
+      <i class="material-icons notranslate">message</i> Memo:&nbsp;
+      {{ transaction.memo }}
+    </p>
+    <p>
+      Fee:
+      <b>{{ transaction.fee.amount }}</b>
+      <span> {{ transaction.fee.denom }}</span>
+    </p>
+    <p>
+      Hash: <span class="hash">{{ transaction.hash }}</span>
+    </p>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex"
+import gql from "graphql-tag"
+import moment from "moment"
+import { atoms, viewDenom } from "scripts/num.js"
+import { prettyInt } from "scripts/num"
+
+export default {
+  name: `transaction-metadata`,
+  filters: {
+    atoms,
+    viewDenom,
+    prettyInt
+  },
+  props: {
+    transaction: {
+      type: Object,
+      required: true
+    }
+  },
+  data: () => ({
+    network: {}
+  }),
+  computed: {
+    ...mapGetters({ networkId: `network` }),
+    date() {
+      const momentTime = moment(this.transaction.timestamp)
+      return momentTime.format(`HH:mm:ss`)
+    }
+  },
+  methods: {
+    getUndelegationEndTime() {
+      return moment(new Date(this.transaction.undelegationEndTime))
+    },
+    checkFeatureAvailable() {
+      const feature = `feature_explorer`
+      return this.network[feature] === true
+    }
+  },
+  apollo: {
+    network: {
+      query: gql`
+        query NetworkActionModal($networkId: String!) {
+          network(id: $networkId) {
+            id
+            feature_explorer
+          }
+        }
+      `,
+      variables() {
+        /* istanbul ignore next */
+        return {
+          networkId: this.networkId
+        }
+      },
+      update(data) {
+        /* istanbul ignore next */
+        return data.network
+      }
+    }
+  }
+}
+</script>
+<style scoped>
+.material-icons {
+  font-size: 1rem;
+  vertical-align: middle;
+  margin-bottom: 2px;
+}
+
+.hash {
+  color: var(--dim);
+  word-break: break-all;
+}
+</style>
---
+++ src/gql/index.js
@@ -0,0 +1,194 @@
+/* istanbul ignore file */
+
+import gql from "graphql-tag"
+import store from "../vuex/store"
+
+function getCurrentNetwork() {
+  // console.log(store())
+  return store().state.connection.network
+}
+
+export const schemaMap = {
+  cosmoshub: "",
+  [`gaia-testnet`]: "gaia_testnet_",
+  testnet: "gaia_testnet_"
+}
+
+export const ValidatorFragment = `
+  name
+  operatorAddress
+  consensusPubkey
+  jailed
+  picture
+  details
+  website
+  identity
+  votingPower
+  startHeight
+  uptimePercentage
+  tokens
+  commissionUpdateTime
+  commission
+  maxCommission
+  maxChangeCommission
+  status
+  statusDetailed
+  expectedReturns
+  selfStake
+`
+
+export const AllValidators = () => {
+  const currentNetwork = getCurrentNetwork()
+  // console.log(`currentNetwork`, currentNetwork)
+  return gql`
+    query AllValidators {
+      validators(networkId: "${currentNetwork}") {
+        ${ValidatorFragment}
+      }
+    }`
+}
+
+export const ValidatorProfile = gql`
+  query validator($networkId: String!, $operatorAddress: String!) {
+    validator(networkId: $networkId, operatorAddress: $operatorAddress) {
+      ${ValidatorFragment}
+    }
+  }
+`
+
+export const DelegatorValidators = schema => gql`
+  query ValidatorInfo($delegatorAddress: String!) {
+    validators(networkId: "${schema}", delegatorAddress: $delegatorAddress) {
+      ${ValidatorFragment}
+    }
+  }
+`
+
+export const DelegationsForDelegator = schema => gql`
+  query Delegations($delegatorAddress: String!) {
+    delegations(networkId: "${schema}", delegatorAddress: $delegatorAddress) {
+      validator {
+        ${ValidatorFragment}
+      }
+      amount
+    }
+  }
+`
+
+export const Networks = gql`
+  query Networks {
+    networks {
+      id
+      chain_id
+      testnet
+      title
+      icon
+    }
+  }
+`
+
+// capability is 'feature_portfolio' / 'action_send'
+export const NetworkCapability = networkId => gql`
+query Networks {
+  network(id: "${networkId}") {
+    id
+    feature_session
+    feature_portfolio
+    feature_validators
+    feature_proposals
+    feature_activity
+    feature_explorer
+    action_send
+    action_claim_rewards
+    action_delegate
+    action_redelegate
+    action_undelegate
+    action_deposit
+    action_vote
+    action_proposal
+    stakingDenom
+  }
+}
+`
+
+export const NetworkCapabilityResult = action => data => data.network[action]
+export const NetworksResult = data => data.networks
+
+const ProposalFragment = `
+  id
+  type
+  title
+  description
+  creationTime
+  status
+  statusBeginTime
+  statusEndTime
+  tally {
+    yes
+    no
+    veto
+    abstain
+    total
+    totalVotedPercentage
+  }
+  deposit
+  proposer,
+  validator {
+    name
+  }
+`
+
+export const ProposalItem = schema => gql`
+  query proposal($id: Int!) {
+    proposal(networkId: "${schema}", id: $id) {
+      ${ProposalFragment}
+    }
+  }
+`
+
+export const GovernanceParameters = schema => gql`
+query governanceParameters {
+  governanceParameters(networkId: "${schema}") {
+    depositDenom
+    votingThreshold
+    vetoThreshold
+    depositThreshold
+  }
+}
+`
+
+export const Vote = schema => gql`
+query vote($proposalId: Int!, $address: String!) {
+  vote(networkId: "${schema}", proposalId: $proposalId, address: $address) {
+    option
+  }
+}
+`
+
+export const Block = networkId => gql`
+query Block {
+  block(networkId: "${networkId}") {
+    height
+    chainId
+  }
+}
+`
+
+export const MetaData = schema => gql`
+query metaData {
+  metaData(networkId: "${schema}") {
+    stakingDenom
+  }
+}
+`
+
+export const UserTransactionAdded = gql`
+  subscription($networkId: String!, $address: String!) {
+    userTransactionAdded(networkId: $networkId, address: $address) {
+      hash
+      height
+      success
+      log
+    }
+  }
+`
