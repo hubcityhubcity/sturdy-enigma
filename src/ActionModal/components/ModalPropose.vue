46e1829afe09321c12aad8fca2afd58357da8930
hubcityhubcity <100476036+hubcityhubcity@users.noreply.github.com>
2022-02-26 14:58:51 -0600

    Initialize ana/possible-fix-for-chunks

---
+++ src/ActionModal/components/ModalPropose.vue
@@ -0,0 +1,268 @@
+<template>
+  <ActionModal
+    id="modal-propose"
+    ref="actionModal"
+    :validate="validateForm"
+    :amount="amount"
+    title="Create Proposal"
+    submission-error-prefix="Submitting proposal failed"
+    :transaction-data="transactionData"
+    :notify-message="notifyMessage"
+    feature-flag="proposal"
+    @close="clear"
+    @txIncluded="onSuccess"
+  >
+    <TmFormGroup
+      :error="$v.title.$error && $v.title.$invalid"
+      class="action-modal-form-group"
+      field-id="title"
+      field-label="Title"
+    >
+      <TmField
+        id="title"
+        v-model.trim="title"
+        v-focus
+        type="text"
+        @keyup.enter.native="refocusOn"
+      />
+      <TmFormMsg
+        v-if="$v.title.$error && !$v.title.maxLength"
+        :max="$v.title.$params.maxLength.max"
+        name="Proposal Title"
+        type="maxLength"
+      />
+      <TmFormMsg
+        v-if="$v.title.$error && !$v.title.required"
+        name="Proposal Title"
+        type="required"
+      />
+    </TmFormGroup>
+    <TmFormGroup
+      :error="$v.description.$error && $v.description.$invalid"
+      class="action-modal-form-group"
+      field-id="description"
+      field-label="Description"
+    >
+      <TmField
+        id="description"
+        ref="description"
+        v-model.trim="description"
+        type="textarea"
+        class="textarea-large"
+      />
+      <TmFormMsg
+        v-if="$v.description.$error && !$v.description.maxLength"
+        :max="$v.description.$params.maxLength.max"
+        name="Description"
+        type="maxLength"
+      />
+      <TmFormMsg
+        v-if="$v.description.$error && !$v.description.required"
+        name="Description"
+        type="required"
+      />
+    </TmFormGroup>
+    <TmFormGroup
+      :error="$v.amount.$error && $v.amount.$invalid"
+      class="action-modal-form-group"
+      field-id="amount"
+      field-label="Deposit"
+    >
+      <span class="input-suffix">{{ denom }}</span>
+      <TmField
+        id="amount"
+        v-model="amount"
+        :value="Number(amount)"
+        type="number"
+        placeholder="0"
+        @keyup.enter.native="enterPressed"
+      />
+      <TmFormMsg
+        v-if="balance.amount === 0"
+        :msg="`doesn't have any ${denom}s`"
+        name="Wallet"
+        type="custom"
+      />
+      <TmFormMsg
+        v-else-if="$v.amount.$error && (!$v.amount.required || amount === 0)"
+        name="Deposit"
+        type="required"
+      />
+      <TmFormMsg
+        v-else-if="$v.amount.$error && !$v.amount.decimal"
+        name="Deposit"
+        type="numberic"
+      />
+      <TmFormMsg
+        v-else-if="$v.amount.$error && !$v.amount.between"
+        :max="$v.amount.$params.between.max"
+        :min="$v.amount.$params.between.min"
+        name="Deposit"
+        type="between"
+      />
+    </TmFormGroup>
+  </ActionModal>
+</template>
+
+<script>
+import { mapGetters } from "vuex"
+import gql from "graphql-tag"
+import {
+  minLength,
+  maxLength,
+  required,
+  between,
+  decimal
+} from "vuelidate/lib/validators"
+import { uatoms, SMALLEST } from "scripts/num"
+import isEmpty from "lodash.isempty"
+import trim from "lodash.trim"
+import TmField from "common/TmField"
+import TmFormGroup from "common/TmFormGroup"
+import TmFormMsg from "common/TmFormMsg"
+import ActionModal from "./ActionModal"
+import { toMicroDenom } from "src/scripts/common"
+
+import transaction from "../utils/transactionTypes"
+
+const isValid = type =>
+  type === `Text` || type === `ParameterChange` || type === `SoftwareUpgrade`
+
+const notBlank = text => !isEmpty(trim(text))
+
+export default {
+  name: `modal-propose`,
+  components: {
+    ActionModal,
+    TmField,
+    TmFormGroup,
+    TmFormMsg
+  },
+  props: {
+    denom: {
+      type: String,
+      required: true
+    }
+  },
+  data: () => ({
+    titleMaxLength: 64,
+    descriptionMaxLength: 10000,
+    title: ``,
+    description: ``,
+    type: `Text`,
+    amount: null,
+    balance: {
+      amount: null,
+      denom: ``
+    }
+  }),
+  computed: {
+    ...mapGetters([`network`]),
+    ...mapGetters({ userAddress: `address` }),
+    transactionData() {
+      return {
+        type: transaction.SUBMIT_PROPOSAL,
+        proposalType: this.type,
+        title: this.title,
+        description: this.description,
+        initialDeposits: [
+          {
+            amount: uatoms(this.amount),
+            denom: toMicroDenom(this.denom)
+          }
+        ]
+      }
+    },
+    notifyMessage() {
+      return {
+        title: `Successful proposal submission!`,
+        body: `You have successfully submitted a new ${this.type.toLowerCase()} proposal`
+      }
+    }
+  },
+  validations() {
+    return {
+      title: {
+        required,
+        minLength: minLength(1),
+        maxLength: maxLength(this.titleMaxLength),
+        notBlank
+      },
+      description: {
+        required,
+        minLength: minLength(1),
+        maxLength: maxLength(this.descriptionMaxLength),
+        notBlank
+      },
+      type: {
+        isValid
+      },
+      amount: {
+        required: x => !!x && x !== `0`,
+        decimal,
+        between: between(SMALLEST, this.balance.amount)
+      }
+    }
+  },
+  methods: {
+    open() {
+      this.$refs.actionModal.open()
+    },
+    validateForm() {
+      this.$v.$touch()
+
+      return !this.$v.$invalid
+    },
+    clear() {
+      this.$v.$reset()
+
+      this.title = ``
+      this.description = ``
+      this.amount = 0
+    },
+    refocusOn() {
+      this.$refs.description.$el.focus()
+    },
+    enterPressed() {
+      this.$refs.actionModal.validateChangeStep()
+    },
+    onSuccess(event) {
+      this.$emit(`success`, event)
+    }
+  },
+  apollo: {
+    balance: {
+      query: gql`
+        query BalanceModalPropos(
+          $networkId: String!
+          $address: String!
+          $denom: String!
+        ) {
+          balance(networkId: $networkId, address: $address, denom: $denom) {
+            amount
+            denom
+          }
+        }
+      `,
+      skip() {
+        return !this.userAddress
+      },
+      variables() {
+        return {
+          networkId: this.network,
+          address: this.userAddress,
+          denom: this.denom
+        }
+      },
+      update(data) {
+        return data.balance || { amount: 0 }
+      }
+    }
+  }
+}
+</script>
+<style>
+.textarea-large {
+  min-height: 200px;
+}
+</style>
---
+++ src/components/common/SessionFrame.vue
@@ -0,0 +1,54 @@
+<template>
+  <transition name="component-fade" mode="out-in">
+    <div class="session-frame">
+      <router-link to="/">
+        <img class="session-logo" src="~assets/images/lunie-logo-white.svg" />
+      </router-link>
+      <div class="session-outer-container">
+        <div class="session">
+          <a v-if="!hideBack" @click="goBack">
+            <i class="material-icons circle back">arrow_back</i>
+          </a>
+          <slot></slot>
+        </div>
+      </div>
+      <div class="session-close">
+        <TmBtn
+          class="session-close-button"
+          value="Back to Lunie"
+          color="secondary"
+          @click.native="$router.push(`/`)"
+        />
+        <a class="user-box" @click="$router.push(`/`)">
+          <i class="material-icons">close</i>
+        </a>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+import TmBtn from "common/TmBtn"
+
+export default {
+  name: `session-frame`,
+  components: {
+    TmBtn
+  },
+  props: {
+    hideBack: {
+      type: Boolean,
+      default: false
+    }
+  },
+  methods: {
+    goBack() {
+      this.$router.go(`-1`)
+    }
+  }
+}
+</script>
+
+<style>
+@import "../../styles/session.css";
+</style>
