1c7214c69ff6c7c0bb1e3a5601c4005ad7ac6c93
hubcityhubcity <100476036+hubcityhubcity@users.noreply.github.com>
2022-02-26 14:57:36 -0600

    Initialize release-ios

---
+++ src/components/common/TmSessionImport.vue
@@ -0,0 +1,87 @@
+<template>
+  <SessionFrame>
+    <TmFormStruct :submit="onSubmit">
+      <h2 class="session-title">
+        Recover with backup code
+      </h2>
+      <div class="session-main">
+        <Steps :steps="[`Recover`, `Name`, `Password`]" active-step="Recover" />
+        <TmFormGroup
+          :error="$v.$error && $v.seed.$invalid"
+          field-id="import-seed"
+          field-label="Backup code"
+        >
+          <FieldSeed
+            id="import-seed"
+            :value="seed"
+            placeholder="Must be exactly 24 words"
+            @input="val => (seed = val)"
+          />
+          <TmFormMsg
+            v-if="$v.seed.$error && !$v.seed.required"
+            name="Seed"
+            type="required"
+          />
+          <TmFormMsg
+            v-else-if="$v.seed.$error && !$v.seed.words24"
+            name="Seed"
+            type="words24"
+          />
+        </TmFormGroup>
+      </div>
+      <div class="session-footer">
+        <TmBtn value="Next" type="submit" />
+      </div>
+    </TmFormStruct>
+  </SessionFrame>
+</template>
+
+<script>
+import { required } from "vuelidate/lib/validators"
+import TmBtn from "common/TmBtn"
+import TmFormGroup from "common/TmFormGroup"
+import TmFormStruct from "common/TmFormStruct"
+import TmFormMsg from "common/TmFormMsg"
+import FieldSeed from "common/TmFieldSeed"
+import SessionFrame from "common/SessionFrame"
+import { mapGetters } from "vuex"
+import Steps from "../../ActionModal/components/Steps"
+
+const words24 = param => {
+  return param && param.split(` `).length === 24
+}
+
+export default {
+  name: `session-import-backupcode`,
+  components: {
+    TmBtn,
+    SessionFrame,
+    FieldSeed,
+    TmFormGroup,
+    TmFormMsg,
+    TmFormStruct,
+    Steps
+  },
+  computed: {
+    ...mapGetters([`recover`]),
+    seed: {
+      get() {
+        return this.$store.state.recover.seed
+      },
+      set(value) {
+        this.$store.commit(`updateField`, { field: `seed`, value })
+      }
+    }
+  },
+  methods: {
+    onSubmit() {
+      this.$v.$touch()
+      if (this.$v.seed.$invalid || this.$v.seed.$invalid) return
+      this.$router.push("/recover/name")
+    }
+  },
+  validations: () => ({
+    seed: { required, words24 }
+  })
+}
+</script>
---
+++ src/vuex/modules/connection.js
@@ -0,0 +1,42 @@
+import config from "src/../config"
+
+export default function() {
+  const state = {
+    stopConnecting: false,
+    connected: true, // TODO do connection test
+    network: config.network, // network id to reference network capabilities stored in Hasura
+    nodeUrl: config.stargate,
+    externals: {
+      config
+    }
+  }
+
+  const mutations = {
+    setNetworkId(state, networkId) {
+      state.network = networkId
+    }
+  }
+
+  const actions = {
+    async checkForPersistedNetwork({ commit }) {
+      const network = localStorage.getItem(`network`)
+      if (network) {
+        await commit(`setNetworkId`, JSON.parse(network))
+      }
+    },
+    async persistNetwork(store, network) {
+      localStorage.setItem(`network`, JSON.stringify(network.id))
+    },
+    async setNetwork({ commit, dispatch }, network) {
+      dispatch(`persistNetwork`, network)
+      commit("setNetworkId", network.id)
+      console.info(`Connecting to: ${network.id}`)
+    }
+  }
+
+  return {
+    state,
+    mutations,
+    actions
+  }
+}
