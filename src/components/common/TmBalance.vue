2206e6f0c86fbe393e339aafab060c93843d3753
hubcityhubcity <100476036+hubcityhubcity@users.noreply.github.com>
2022-02-26 14:58:10 -0600

    Initialize ana/fix-query-balances-in-tmbalance

---
+++ src/components/common/TmBalance.vue
@@ -0,0 +1,318 @@
+<template>
+  <div class="balance-header">
+    <div
+      v-if="$apollo.queries.overview.loading && !overview.totalStake"
+      class="loading-image-container"
+    >
+      <img
+        src="/img/balance-header-loading.svg"
+        alt="geometric placeholder shapes"
+      />
+    </div>
+    <div v-else>
+      <div class="values-container">
+        <div class="total-atoms">
+          <h3>Total {{ stakingDenom }}</h3>
+          <h2 class="total-atoms__value">
+            {{ overview.totalStake | shortDecimals | noBlanks }}
+          </h2>
+        </div>
+
+        <div class="row small-container">
+          <div v-if="overview.totalStake > 0" class="available-atoms">
+            <h3>Available {{ stakingDenom }}</h3>
+            <h2>{{ overview.liquidStake | shortDecimals | noBlanks }}</h2>
+          </div>
+
+          <div v-if="overview.totalRewards" class="rewards">
+            <h3>Total Rewards</h3>
+            <h2>+{{ overview.totalRewards | shortDecimals | noBlanks }}</h2>
+          </div>
+        </div>
+      </div>
+      <div class="button-container">
+        <TmBtn
+          class="send-button"
+          value="Send"
+          type="secondary"
+          @click.native="onSend()"
+        />
+        <TmBtn
+          id="withdraw-btn"
+          :disabled="!readyToWithdraw"
+          class="withdraw-rewards"
+          value="Claim Rewards"
+          @click.native="readyToWithdraw && onWithdrawal()"
+        />
+      </div>
+
+      <SendModal ref="SendModal" :denoms="getAllDenoms" :balances="balances" />
+      <ModalWithdrawRewards ref="ModalWithdrawRewards" />
+    </div>
+  </div>
+</template>
+<script>
+import { shortDecimals } from "scripts/num"
+import { noBlanks } from "src/filters"
+import TmBtn from "common/TmBtn"
+import SendModal from "src/ActionModal/components/SendModal"
+import ModalWithdrawRewards from "src/ActionModal/components/ModalWithdrawRewards"
+import { mapGetters } from "vuex"
+import gql from "graphql-tag"
+export default {
+  name: `tm-balance`,
+  components: {
+    TmBtn,
+    SendModal,
+    ModalWithdrawRewards
+  },
+  filters: {
+    shortDecimals,
+    noBlanks
+  },
+  data() {
+    return {
+      overview: {},
+      stakingDenom: "",
+      balances: []
+    }
+  },
+  computed: {
+    ...mapGetters([`address`, `network`]),
+    // only be ready to withdraw of the validator rewards are loaded and the user has rewards to withdraw
+    // the validator rewards are needed to filter the top 5 validators to withdraw from
+    readyToWithdraw() {
+      return this.overview.totalRewards > 0
+    },
+    getAllDenoms() {
+      if (this.balances) {
+        const balances = this.balances
+        return balances.map(({ denom }) => denom)
+      } else {
+        return [this.stakingDenom]
+      }
+    }
+  },
+  methods: {
+    onWithdrawal() {
+      this.$refs.ModalWithdrawRewards.open()
+    },
+    onSend() {
+      this.$refs.SendModal.open()
+    }
+  },
+  apollo: {
+    overview: {
+      query: gql`
+        query overview($networkId: String!, $address: String!) {
+          overview(networkId: $networkId, address: $address) {
+            totalRewards
+            liquidStake
+            totalStake
+          }
+        }
+      `,
+      variables() {
+        /* istanbul ignore next */
+        return {
+          networkId: this.network,
+          address: this.address
+        }
+      },
+      update(data) {
+        /* istanbul ignore next */
+        return {
+          ...data.overview,
+          totalRewards: Number(data.overview.totalRewards)
+        }
+      },
+      skip() {
+        /* istanbul ignore next */
+        return !this.address
+      }
+    },
+    balances: {
+      query: gql`
+        query BalancesSendModal($networkId: String!, $address: String!) {
+          balances(networkId: $networkId, address: $address) {
+            amount
+            denom
+          }
+        }
+      `,
+      skip() {
+        /* istanbul ignore next */
+        return !this.address
+      },
+      variables() {
+        /* istanbul ignore next */
+        return {
+          networkId: this.network,
+          address: this.address
+        }
+      },
+      update(data) {
+        /* istanbul ignore next */
+        return data.balances
+      }
+    },
+    stakingDenom: {
+      query: gql`
+        query Network($networkId: String!) {
+          network(id: $networkId) {
+            id
+            stakingDenom
+          }
+        }
+      `,
+      variables() {
+        /* istanbul ignore next */
+        return {
+          networkId: this.network
+        }
+      },
+      update(data) {
+        /* istanbul ignore next */
+        return data.network.stakingDenom
+      }
+    },
+    $subscribe: {
+      blockAdded: {
+        variables() {
+          /* istanbul ignore next */
+          return {
+            networkId: this.network
+          }
+        },
+        query() {
+          /* istanbul ignore next */
+          return gql`
+            subscription($networkId: String!) {
+              blockAdded(networkId: $networkId) {
+                height
+                chainId
+              }
+            }
+          `
+        },
+        result() {
+          /* istanbul ignore next */
+          this.$apollo.queries.overview.refetch()
+        }
+      }
+    }
+  }
+}
+</script>
+<style scoped>
+.balance-header {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+}
+
+.values-container {
+  display: flex;
+  position: relative;
+  width: 100%;
+  padding: 1rem 2rem;
+  flex-direction: column;
+}
+
+.values-container h2 {
+  font-size: 24px;
+  font-weight: 500;
+  line-height: 24px;
+  color: var(--bright);
+}
+
+.values-container h3 {
+  font-size: var(--sm);
+  font-weight: 400;
+  white-space: nowrap;
+}
+
+.total-atoms,
+.available-atoms,
+.rewards {
+  padding-right: 2.5rem;
+}
+
+.rewards h2 {
+  color: var(--success);
+  font-size: var(--m);
+}
+
+.available-atoms h2 {
+  font-size: var(--m);
+  line-height: 20px;
+}
+
+.button-container {
+  display: flex;
+  align-items: center;
+  padding: 0.5rem 2rem;
+  width: 100%;
+  border-bottom: 1px solid var(--bc-dim);
+  border-top: 1px solid var(--bc-dim);
+  margin-bottom: 2rem;
+}
+
+.button-container button:first-child {
+  margin-right: 0.5rem;
+}
+
+.row {
+  display: flex;
+  flex-direction: row;
+}
+
+.small-container {
+  padding-top: 1rem;
+}
+
+@media screen and (max-width: 667px) {
+  .balance-header {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .values-container {
+    flex-direction: column;
+    width: 100%;
+  }
+
+  .values-container .total-atoms__value {
+    font-size: 28px;
+    font-weight: 500;
+    line-height: 32px;
+  }
+
+  .available-atoms,
+  .rewards {
+    padding: 0;
+  }
+
+  .total-atoms {
+    padding: 1rem 0;
+    text-align: center;
+  }
+
+  .button-container {
+    width: 100%;
+    padding: 1rem;
+    border-top: 1px solid var(--bc);
+  }
+
+  .button-container button {
+    width: 50%;
+  }
+
+  .small-container {
+    display: flex;
+    justify-content: space-evenly;
+    padding: 1rem 0;
+    text-align: center;
+  }
+}
+</style>
