062fcfc3eacaa474e31ac35f75d860d556f786ff
hubcityhubcity <100476036+hubcityhubcity@users.noreply.github.com>
2022-02-26 15:02:08 -0600

    Initialize 4017-polkadot-address-role-2

---
+++ src/components/wallet/PagePortfolio.vue
@@ -0,0 +1,30 @@
+<template>
+  <TmPage :sign-in-required="true" :managed="true" :dark-background="true">
+    <template v-if="!isRestrictedAddressRole" slot="managed-body">
+      <DelegationsOverview />
+      <Undelegations />
+    </template>
+  </TmPage>
+</template>
+
+<script>
+import TmPage from "common/TmPage"
+import DelegationsOverview from "staking/DelegationsOverview"
+import Undelegations from "staking/Undelegations"
+import { mapState } from "vuex"
+
+export default {
+  name: `page-portfolio`,
+  components: {
+    TmPage,
+    Undelegations,
+    DelegationsOverview
+  },
+  computed: {
+    ...mapState([`session`]),
+    isRestrictedAddressRole() {
+      return this.session.accountRole === "controller"
+    }
+  }
+}
+</script>
