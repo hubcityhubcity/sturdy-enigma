e40b7cfa815d59bbdbe18d707b542b8513bf0684
hubcityhubcity <100476036+hubcityhubcity@users.noreply.github.com>
2022-02-26 15:07:41 -0600

    Initialize ana/fix-url-img-import-in-extension

---
+++ app/src/components/governance/PageProposal.vue
@@ -0,0 +1,484 @@
+<template>
+  <TmPage
+    data-title="Proposal"
+    class="readable-width"
+    :loading="$apollo.queries.proposal.loading"
+  >
+    <TmDataNotFound v-if="!found" />
+    <template v-else>
+      <div class="proposal">
+        <div class="page-profile__header__info">
+          <span :class="proposal.status | lowerCase" class="proposal-status">
+            {{ status.badge }}
+          </span>
+          <div class="proposal-title__row">
+            <h2 class="proposal-title">{{ proposal.title }}</h2>
+          </div>
+        </div>
+
+        <div class="proposer-row">
+          <p class="proposer">
+            <template v-if="proposal.validator">
+              Proposed by {{ proposal.validator.name }}:
+              <Address :address="proposal.proposer" />
+            </template>
+            <template v-else-if="proposal.proposer">
+              Proposed by
+              <Address :address="proposal.proposer" />
+            </template>
+            <template v-else>
+              Unknown proposer
+            </template>
+          </p>
+          <div class="button-container">
+            <TmBtn
+              v-if="proposal.status !== 'Passed'"
+              id="deposit-btn"
+              value="Deposit"
+              :disabled="proposal.status !== 'DepositPeriod'"
+              color="primary"
+              @click.native="onDeposit"
+            />
+            <TmBtn
+              id="vote-btn"
+              value="Vote"
+              :disabled="
+                proposal.status !== 'VotingPeriod' ||
+                proposal.type === 'COUNCIL'
+              "
+              color="primary"
+              @click.native="() => onVote()"
+            />
+          </div>
+        </div>
+      </div>
+
+      <TextBlock :content="proposal.description" />
+
+      <ul v-if="proposal.status === 'DepositPeriod'" class="row">
+        <li>
+          <h4>Deposit Count</h4>
+          <span>
+            {{ proposal.deposit }}
+            /
+            {{ parameters.depositThreshold }}
+            {{ parameters.depositDenom || currentNetwork.stakingDenom }}
+          </span>
+        </li>
+      </ul>
+
+      <ul v-if="proposal.status !== `DepositPeriod`" class="row">
+        <li v-if="proposal.status === `VotingPeriod`">
+          <h4>Total Vote Count</h4>
+          <span>
+            {{ proposal.tally.totalVotedPercentage | percent }} /
+            {{ proposal.tally.total | prettyInt }}
+          </span>
+        </li>
+        <li>
+          <h4>Yes</h4>
+          <span>
+            {{
+              noVotes
+                ? 0
+                : (proposal.tally.yes / proposal.tally.total) | percent
+            }}
+            /
+            {{ proposal.tally.yes | prettyInt }}
+          </span>
+        </li>
+        <li>
+          <h4>No</h4>
+          <span>
+            {{
+              noVotes ? 0 : (proposal.tally.no / proposal.tally.total) | percent
+            }}
+            /
+            {{ proposal.tally.no | prettyInt }}
+          </span>
+        </li>
+        <li>
+          <h4>Veto</h4>
+          <span>
+            {{
+              noVotes
+                ? 0
+                : (proposal.tally.veto / proposal.tally.total) | percent
+            }}
+            /
+            {{ proposal.tally.veto | prettyInt }}
+          </span>
+        </li>
+        <li>
+          <h4>Abstain</h4>
+          <span>
+            {{
+              noVotes
+                ? 0
+                : (proposal.tally.abstain / proposal.tally.total) | percent
+            }}
+            /
+            {{ proposal.tally.abstain | prettyInt }}
+          </span>
+        </li>
+      </ul>
+
+      <ul class="row">
+        <li>
+          <h4>Proposal ID</h4>
+          <span>{{ proposal.id }}</span>
+        </li>
+        <li>
+          <h4>Submitted</h4>
+          <span v-if="proposal.creationTime">{{
+            proposal.creationTime | date
+          }}</span>
+          <span v-else>--</span>
+        </li>
+        <template
+          v-if="['DepositPeriod', 'VotingPeriod'].includes(proposal.status)"
+        >
+          <li>
+            <h4>({{ status.badge }}) Start Date</h4>
+            <span v-if="proposal.statusBeginTime">{{
+              proposal.statusBeginTime | date
+            }}</span>
+            <span v-else>--</span>
+          </li>
+          <li>
+            <h4>({{ status.badge }}) End Date</h4>
+            <span v-if="proposal.statusEndTime">
+              {{ proposal.statusEndTime | date }} /
+              {{ proposal.statusEndTime | fromNow }}
+            </span>
+            <span v-else>--</span>
+          </li>
+        </template>
+        <template v-else>
+          <li>
+            <h4>Proposal Finalized ({{ status.badge }})</h4>
+            <span>{{ proposal.statusEndTime | date }}</span>
+          </li>
+        </template>
+      </ul>
+
+      <ModalDeposit
+        v-if="parameters.depositDenom"
+        ref="modalDeposit"
+        :proposal-id="proposalId"
+        :proposal-title="proposal.title || ''"
+        :denom="parameters.depositDenom"
+        @success="() => afterDeposit()"
+      />
+      <ModalVoteSubstrate
+        v-if="currentNetwork.network_type === `polkadot`"
+        ref="modalVote"
+        :proposal-id="proposal.proposalId"
+        :proposal-title="proposal.title || ''"
+        :last-vote-option="vote"
+        @success="() => afterVote()"
+      />
+      <ModalVote
+        v-else
+        ref="modalVote"
+        :proposal-id="proposalId"
+        :proposal-title="proposal.title || ''"
+        :last-vote-option="vote"
+        @success="() => afterVote()"
+      />
+    </template>
+  </TmPage>
+</template>
+
+<script>
+import { mapGetters } from "vuex"
+import { percent, prettyInt } from "scripts/num"
+import { date, fromNow } from "src/filters"
+import TmBtn from "common/TmBtn"
+import TmDataNotFound from "common/TmDataNotFound"
+import TextBlock from "common/TextBlock"
+import ModalDeposit from "src/ActionModal/components/ModalDeposit"
+import ModalVote from "src/ActionModal/components/ModalVote"
+import ModalVoteSubstrate from "src/ActionModal/components/ModalVoteSubstrate"
+import TmPage from "common/TmPage"
+import { getProposalStatus } from "scripts/proposal-status"
+import { ProposalItem, GovernanceParameters, Vote } from "src/gql"
+import BigNumber from "bignumber.js"
+import Address from "common/Address"
+import gql from "graphql-tag"
+
+export default {
+  name: `page-proposal`,
+  components: {
+    TmBtn,
+    ModalDeposit,
+    ModalVote,
+    ModalVoteSubstrate,
+    TmDataNotFound,
+    TmPage,
+    TextBlock,
+    Address,
+  },
+  filters: {
+    prettyInt,
+    percent,
+    date,
+    fromNow,
+    lowerCase: (text) => (text ? text.toLowerCase() : ""),
+  },
+  props: {
+    proposalId: {
+      type: String,
+      required: true,
+    },
+  },
+  data: () => ({
+    proposals: [],
+    vote: undefined,
+    proposal: {
+      status: "",
+      proposer: "",
+      tally: {},
+      validator: {},
+    },
+    parameters: {
+      depositDenom: "",
+    },
+    error: undefined,
+    found: false,
+    loaded: false,
+  }),
+  computed: {
+    ...mapGetters([`address`, `network`, `currentNetwork`]),
+    status() {
+      return getProposalStatus(this.proposal)
+    },
+    noVotes() {
+      return BigNumber(this.proposal.tally.total).eq(0)
+    },
+    getNextProposalId() {
+      let id = this.getProposalIndex(-1)
+      return id
+    },
+    getPrevProposalId() {
+      let id = this.getProposalIndex(1)
+      return id
+    },
+  },
+  watch: {
+    // Needed to show data loading component when you are browsing from one proposal to another
+    $route: function () {
+      this.loaded = false
+    },
+  },
+  methods: {
+    onVote() {
+      this.$refs.modalVote.open()
+    },
+    afterVote() {
+      this.$apollo.queries.vote.refetch()
+    },
+    onDeposit() {
+      this.$refs.modalDeposit.open()
+    },
+    afterDeposit() {
+      this.$apollo.queries.proposal.refetch()
+    },
+    getProposalIndex(num) {
+      let proposalsObj = this.proposals
+      let proposalsIdArr = Object.values(proposalsObj).map(
+        (proposal) => proposal.id
+      )
+      return proposalsIdArr[proposalsIdArr.indexOf(this.proposal.id) + num]
+    },
+  },
+  apollo: {
+    proposal: {
+      /* istanbul ignore next */
+      query() {
+        return ProposalItem(this.network)
+      },
+      /* istanbul ignore next */
+      update(data) {
+        this.loaded = true
+        if (data.proposal) this.found = true
+        return data.proposal || {}
+      },
+      /* istanbul ignore next */
+      variables() {
+        return {
+          id: this.proposalId,
+        }
+      },
+      /* istanbul ignore next */
+      result(data) {
+        if (data.proposal) this.found = true
+        this.error = data.error
+      },
+    },
+    parameters: {
+      /* istanbul ignore next */
+      query() {
+        return GovernanceParameters(this.network)
+      },
+      /* istanbul ignore next */
+      update(data) {
+        return data.governanceParameters || {}
+      },
+      /* istanbul ignore next */
+      skip() {
+        // only Tendermint networks have this network-wide "governance parameters" logic
+        return !this.found || this.currentNetwork.network_type !== `cosmos`
+      },
+      /* istanbul ignore next */
+      result(data) {
+        this.error = data.error
+      },
+    },
+    vote: {
+      /* istanbul ignore next */
+      query() {
+        return Vote(this.network)
+      },
+      /* istanbul ignore next */
+      variables() {
+        return {
+          proposalId: this.proposalId,
+          address: this.address,
+        }
+      },
+      /* istanbul ignore next */
+      skip() {
+        return !this.address || !this.found
+      },
+      update(data) {
+        if (data.vote) return data.vote.option
+        return undefined
+      },
+      /* istanbul ignore next */
+      result(data) {
+        this.error = data.error
+      },
+    },
+    $subscribe: {
+      blockAdded: {
+        /* istanbul ignore next */
+        variables() {
+          return {
+            networkId: this.network,
+          }
+        },
+        /* istanbul ignore next */
+        query() {
+          return gql`
+            subscription($networkId: String!) {
+              blockAdded(networkId: $networkId) {
+                height
+              }
+            }
+          `
+        },
+        /* istanbul ignore next */
+        skip() {
+          return !this.found
+        },
+        /* istanbul ignore next */
+        result() {
+          if (
+            // Don't update passed or rejected proposals
+            this.proposal.status !== "Passed" &&
+            this.proposal.status !== "Rejected" &&
+            this.loaded
+          ) {
+            this.$apollo.queries.proposal.refetch()
+            this.$apollo.queries.parameters.refetch()
+            this.$apollo.queries.vote.refetch()
+          }
+        },
+      },
+    },
+  },
+}
+</script>
+
+<style scoped>
+@import "../../styles/proposal-status.css";
+
+.proposal-title__row {
+  color: var(--bright);
+}
+
+.proposal-title__row a {
+  color: var(--bright);
+  padding-top: 1rem;
+}
+
+.proposal-title__row a:hover {
+  color: var(--link-hover);
+  padding-top: 1rem;
+}
+
+.proposal-title {
+  color: var(--bright);
+  font-size: var(--h1);
+  line-height: 2.25rem;
+  font-weight: 500;
+  padding-top: 2rem;
+}
+
+.proposer {
+  font-size: 12px;
+  color: var(--txt);
+}
+
+.text-block {
+  padding: 0 1rem 3rem;
+}
+
+.proposer-row {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0.5rem 1rem;
+  margin: 2rem 0;
+}
+
+.button-container {
+  display: flex;
+  flex-direction: row;
+}
+
+.page-profile__header__info {
+  padding: 1rem;
+}
+
+.button-container button:first-child {
+  margin-right: 0.5rem;
+}
+
+.read-more-link {
+  padding-top: 1rem;
+  font-size: 14px;
+  display: inline-block;
+  cursor: pointer;
+}
+
+.read-more-link:hover {
+  color: var(--link);
+}
+
+@media screen and (max-width: 667px) {
+  .proposer-row {
+    flex-direction: column;
+  }
+
+  .button-container {
+    width: 100%;
+    padding: 1rem;
+  }
+
+  .button-container button {
+    width: 100%;
+  }
+}
+</style>
---
+++ app/src/components/staking/DelegationsOverview.vue
@@ -0,0 +1,266 @@
+<template>
+  <div class="delegations-overview">
+    <div class="table-container">
+      <div
+        v-if="$apollo.queries.delegations.loading && !delegationsLoaded"
+        class="loading-image-container"
+      >
+        <img
+          class="loading-image"
+          src="/img/portfolio-loading.svg"
+          alt="geometric placeholder shapes"
+        />
+      </div>
+      <div v-else-if="delegations.length > 0 || stakedBalance.total > 0">
+        <h1>Your Stake</h1>
+        <BalanceRow
+          :balance="stakedBalance"
+          :stake="currentNetwork.network_type === 'polkadot'"
+          :unstake="currentNetwork.network_type === 'polkadot'"
+        />
+        <TableValidators
+          v-if="delegations.length > 0"
+          :validators="delegations.map(({ validator }) => validator)"
+          :delegations="delegations"
+          class="table-validators"
+          show-on-mobile="expectedReturns"
+        />
+      </div>
+      <TmDataMsg
+        v-if="!$apollo.loading && delegations.length === 0"
+        icon="sentiment_dissatisfied"
+      >
+        <div slot="title">No validators in your portfolio</div>
+        <div slot="subtitle">
+          Head over to the
+          <a @click="goToValidators()">validator list</a>&nbsp;to
+          {{
+            stakedBalance.total > 0 ? `start earning rewards` : `get staking`
+          }}!
+        </div>
+      </TmDataMsg>
+      <UndelegationModal ref="UnstakeModal" />
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapState } from "vuex"
+import BalanceRow from "common/BalanceRow"
+import TmDataMsg from "common/TmDataMsg"
+import TableValidators from "staking/TableValidators"
+import UndelegationModal from "src/ActionModal/components/UndelegationModal"
+import {
+  ValidatorFragment,
+  DelegationsForDelegator,
+  UserTransactionAdded,
+} from "src/gql"
+import gql from "graphql-tag"
+
+export default {
+  name: `delegations-overview`,
+  components: {
+    BalanceRow,
+    TableValidators,
+    UndelegationModal,
+    TmDataMsg,
+  },
+  data: () => ({
+    delegations: [],
+    undelegations: [],
+    balances: [],
+    delegationsLoaded: false,
+    undelegationsLoaded: false,
+  }),
+  computed: {
+    ...mapState([`session`]),
+    ...mapGetters([`address`, `currentNetwork`]),
+    stakedBalance() {
+      // balances not loaded yet
+      if (!this.balances.length) {
+        return {
+          total: 0,
+          denom: this.currentNetwork.stakingDenom,
+        }
+      }
+      const stakingDenomBalance = this.balances.find(
+        ({ denom }) => denom === this.currentNetwork.stakingDenom
+      )
+      let stakedAmount =
+        Number(stakingDenomBalance.total) -
+        Number(stakingDenomBalance.available)
+      // substract the already unbonding balance in the case of Substrate networks.
+      if (this.undelegationsLoaded && this.undelegations.length > 0) {
+        stakedAmount = this.undelegations.reduce(
+          (stakedAmount, { amount }) => stakedAmount - Number(amount),
+          stakedAmount
+        )
+      }
+      return {
+        total: stakedAmount.toFixed(3),
+        denom: this.currentNetwork.stakingDenom,
+      }
+    },
+  },
+  methods: {
+    goToValidators() {
+      this.$router.push({
+        name: "validators",
+        params: {
+          networkId: this.currentNetwork.slug,
+        },
+      })
+    },
+    openUnstakeModal() {
+      this.$refs.UnstakeModal.open()
+    },
+  },
+  apollo: {
+    balances: {
+      query: gql`
+        query($networkId: String!, $address: String!) {
+          balancesV2(networkId: $networkId, address: $address) {
+            id
+            type
+            denom
+            available
+            total
+          }
+        }
+      `,
+      /* istanbul ignore next */
+      variables() {
+        return {
+          networkId: this.currentNetwork.id,
+          address: this.address,
+        }
+      },
+      /* istanbul ignore next */
+      skip() {
+        return !this.address
+      },
+      update(result) {
+        return result.balancesV2
+      },
+    },
+    delegations: {
+      query() {
+        /* istanbul ignore next */
+        return DelegationsForDelegator(this.currentNetwork.id)
+      },
+      variables() {
+        /* istanbul ignore next */
+        return {
+          delegatorAddress: this.address,
+          networkId: this.currentNetwork.id,
+        }
+      },
+      /* istanbul ignore next */
+      update(data) {
+        this.delegationsLoaded = true
+        return data.delegations || []
+      },
+    },
+    undelegations: {
+      query() {
+        /* istanbul ignore next */
+        return gql`
+        query undelegations($networkId: String!, $delegatorAddress: String!) {
+          undelegations(networkId: $networkId, delegatorAddress: $delegatorAddress) {
+            id
+            validator {
+              ${ValidatorFragment}
+            }
+            amount
+            startHeight
+            endTime
+          }
+        }
+      `
+      },
+      /* istanbul ignore next */
+      variables() {
+        return {
+          networkId: this.currentNetwork.id,
+          delegatorAddress: this.address,
+        }
+      },
+      /* istanbul ignore next */
+      skip() {
+        return this.currentNetwork.network_type !== `polkadot` // we only need undelegations for Polkadot networks
+      },
+      /* istanbul ignore next */
+      update(data) {
+        this.undelegationsLoaded = true
+        return data.undelegations
+      },
+    },
+    $subscribe: {
+      userTransactionAdded: {
+        variables() {
+          return {
+            networkId: this.currentNetwork.id,
+            address: this.address,
+          }
+        },
+        skip() {
+          return !this.address
+        },
+        query: UserTransactionAdded,
+        result({ data }) {
+          /* istanbul ignore next */
+          if (data.userTransactionAddedV2.success) {
+            this.$apollo.queries.delegations.refetch()
+          }
+        },
+      },
+    },
+  },
+}
+</script>
+<style scoped>
+h1 {
+  font-size: 24px;
+  color: var(--bright);
+  font-weight: 400;
+  padding-bottom: 2rem;
+}
+
+.delegations-overview {
+  background: var(--app-fg);
+}
+
+.table-container {
+  max-width: 1100px;
+  margin: 0 auto;
+  width: 100%;
+  padding: 4rem 2rem;
+}
+
+.table-validators {
+  margin-top: 2rem;
+}
+
+.tm-form-msg--desc {
+  padding-bottom: 1rem;
+}
+
+.tm-data-msg {
+  margin-top: 1rem;
+}
+
+@media screen and (max-width: 667px) {
+  h1 {
+    padding: 2rem;
+    text-align: center;
+  }
+
+  .loading-image-container {
+    padding: 2rem;
+  }
+
+  .table-container {
+    padding: 4rem 1rem;
+  }
+}
+</style>
