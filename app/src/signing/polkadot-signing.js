b9ee1e85c2ed186d8b1459127b1bab3b37834e18
hubcityhubcity <100476036+hubcityhubcity@users.noreply.github.com>
2022-02-26 15:06:19 -0600

    Initialize ana/improve-polkadot-txs-debugging

---
+++ app/src/signing/polkadot-signing.js
@@ -0,0 +1,29 @@
+import { hexToU8a } from "@polkadot/util"
+
+export async function getSignature(
+  { payload, transaction },
+  wallet,
+  network,
+  curve
+) {
+  const [{ Keyring }] = await Promise.all([
+    import("@polkadot/api"),
+    import("@polkadot/wasm-crypto").then(async ({ waitReady }) => {
+      await waitReady()
+    }),
+    import("@polkadot/util-crypto").then(async ({ cryptoWaitReady }) => {
+      // Wait for the promise to resolve, async WASM or `cryptoWaitReady().then(() => { ... })`
+      await cryptoWaitReady()
+    }),
+  ])
+
+  const keyring = new Keyring({
+    ss58Format: Number(network.address_prefix),
+    type: curve || network.defaultCurve,
+  })
+  const keypair = keyring.createFromUri(wallet.seedPhrase)
+
+  const rawSignature = keypair.sign(hexToU8a(payload.toRaw().data))
+
+  return { payload, transaction, rawSignature }
+}
